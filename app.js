12814644fa8448e30c156fe81634432fcb707745
Sabrina Ooi <heysabs@gmail.com>
2016-08-17 11:48:53 +0800

    added mongoose

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // MY MAIN APP JS
 
+// Require mongoose
+var mongoose = require('mongoose');
+var mongo_url = process.env.MONGODB_URI || 'mongodb://localhost/sabrina';
+mongoose.Promise = global.Promise;
+mongoose.connect(mongo_url);
+
+
 var express = require('./config/express');
 var app = express();
 
-app.set('port', (process.env.PORT || 7000));
+var port = process.env.PORT || 7000;
+
+app.set('port', port);
 
 app.listen(app.get('port'), function() {
   console.log('My express server is running at localhost', app.get('port'));
